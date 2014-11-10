b70394d61377f46f5559b7b140e31b0bc3469630
Giorgos Logiotatidis <seadog@sealabs.net>
2014-11-10 15:57:37 +0200

    iii

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 set -e  
 cd $DRONE_BUILD_DIR
 
-# wrapdocker &  
-# sleep 5
-
 export DOCKER_HOST=tcp://178.62.192.52:2375
 docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t giorgos/test-drone:$DRONE_COMMIT .  
-docker push giorgos/test-drone:$DRONE_COMMIT
 
 fig -f ci-fig.yml up
+docker push giorgos/test-drone:$DRONE_COMMIT
