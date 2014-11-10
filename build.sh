0709b57a0b1f172526b975a7716f39cb634f7114
Giorgos Logiotatidis <seadog@sealabs.net>
2014-11-10 16:17:45 +0200

    foo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd $DRONE_BUILD_DIR
 export DOCKER_HOST=tcp://178.62.192.52:2375
 docker build -t giorgos/test-drone:$DRONE_COMMIT .  
 
-sed -i "s/\$TAG\$/$DRONE_COMMIT/g" ci-fig.yml
+sed -i "s/DRONE_TAG/$DRONE_COMMIT/g" ci-fig.yml
 fig -f ci-fig.yml up
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
